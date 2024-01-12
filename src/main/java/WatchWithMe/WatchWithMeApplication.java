0006897f06b55a8554de4fcc4298a4c065a117fc
chanmin-00 <jjw421412@naver.com>
2024-01-12 21:41:52 +0900

    스프링 환경 설정

---
+++ src/main/java/WatchWithMe/WatchWithMeApplication.java
@@ -0,0 +1,13 @@
+package WatchWithMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchWithMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchWithMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WatchWithMe/WatchWithMeApplicationTests.java
@@ -0,0 +1,13 @@
+package WatchWithMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchWithMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
