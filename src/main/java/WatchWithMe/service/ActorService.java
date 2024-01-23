d94187aac1a7aa9506252feca1c7bfc2306cdd97
chanmin-00 <jjw421412@naver.com>
2024-01-23 18:51:09 +0900

    feat: 영화 배우별, 감독별 영화 조회 기능 구현

--- src/main/java/WatchWithMe/service/ActorService.java
+++ src/main/java/WatchWithMe/service/ActorService.java
@@ -1,9 +1,38 @@
 package WatchWithMe.service;
 
+import WatchWithMe.domain.Actor;
+import WatchWithMe.domain.Movie;
+import WatchWithMe.domain.MovieActor;
+import WatchWithMe.dto.request.ActorListRequestDto;
+import WatchWithMe.dto.response.MovieListResponseDto;
+import WatchWithMe.repository.actor.ActorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ActorService {
+
+    private final ActorRepository actorRepository;
+
+    // 배우별 영화 조회
+    public List<MovieListResponseDto> searchMovieListByActor(ActorListRequestDto actorListRequestDto){
+        List<MovieListResponseDto> movieListResponseDtoList = new ArrayList<>();
+
+        List<Actor> actorList = actorRepository.search(actorListRequestDto);
+        for(int i = 0;i < actorList.size();i++){
+            Actor actor = actorList.get(i);
+            List<MovieActor> movieActorList = actor.getMovieActorList();
+            for(int j = 0;j < movieActorList.size();j++){
+                MovieActor movieActor = movieActorList.get(j);
+                Movie movie = movieActor.getMovie();
+                MovieListResponseDto movieListResponseDto = new MovieListResponseDto(movie);
+                movieListResponseDtoList.add(movieListResponseDto);
+            }
+        }
+        return movieListResponseDtoList;
+    }
 }
--- src/main/java/WatchWithMe/service/DirectorService.java
+++ src/main/java/WatchWithMe/service/DirectorService.java
@@ -1,9 +1,36 @@
 package WatchWithMe.service;
 
+import WatchWithMe.domain.*;
+import WatchWithMe.dto.request.DirectorListRequestDto;
+import WatchWithMe.dto.response.MovieListResponseDto;
+import WatchWithMe.repository.director.DirectorRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DirectorService {
+
+    private final DirectorRepository directorRepository;
+
+    // 감독별 영화 조회
+    public List<MovieListResponseDto> searchMovieListByDirector(DirectorListRequestDto directorListRequestDto){
+        List<MovieListResponseDto> movieListResponseDtoList = new ArrayList<>();
+
+        List<Director> directorList = directorRepository.search(directorListRequestDto);
+        for(int i = 0;i < directorList.size();i++){
+            Director director = directorList.get(i);
+            List<MovieDirector> movieDirectorList = director.getMovieDirectorList();
+            for(int j = 0;j < movieDirectorList.size();j++){
+                MovieDirector movieDirector = movieDirectorList.get(j);
+                Movie movie = movieDirector.getMovie();
+                MovieListResponseDto movieListResponseDto = new MovieListResponseDto(movie);
+                movieListResponseDtoList.add(movieListResponseDto);
+            }
+        }
+        return movieListResponseDtoList;
+    }
 }
