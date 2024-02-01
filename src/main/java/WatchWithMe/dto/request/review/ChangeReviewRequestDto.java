49be0cea2000a1b94499bf687c9ba439eaf8a5f9
chanmin-00 <jjw421412@naver.com>
2024-02-01 17:35:31 +0900

    feat: 리뷰 수정 및 삭제 기능 추가

---
+++ src/main/java/WatchWithMe/dto/request/review/ChangeReviewRequestDto.java
@@ -0,0 +1,17 @@
+package WatchWithMe.dto.request.review;
+
+import jakarta.validation.constraints.*;
+
+// 리뷰 수정 Dto
+public record ChangeReviewRequestDto(
+
+        @Size(max = 50, message = "리뷰 작성글은 50자 이내입니다")
+        String reviewText, // review 글
+
+        @Max(value = 5, message = "최댓값은 5입니다")
+        @Min(value = 0, message = "최솟값은 0입니다")
+        Double memberRating, // 평점
+
+        String memberRatingGenre// 장르
+
+) {}
