4ae9fe17911a37c3952c3077f9cf0659ca03deeb
chanmin-00 <jjw421412@naver.com>
2024-02-08 14:54:51 +0900

    feat: 리뷰 조회 페이징 기능 추가

--- src/main/java/WatchWithMe/repository/review/ReviewRepositoryCustom.java
+++ src/main/java/WatchWithMe/repository/review/ReviewRepositoryCustom.java
@@ -1,6 +1,7 @@
 package WatchWithMe.repository.review;
 
 import WatchWithMe.domain.Review;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface ReviewRepositoryCustom {
@@ -8,10 +9,10 @@ public interface ReviewRepositoryCustom {
     /*
     내가 쓴 리뷰 조회
      */
-    List<Review> searchByMember(Long memberId);
+    List<Review> searchByMember(Long memberId, Pageable pageable);
 
     /*
     영화 리뷰 조회
      */
-    List<Review> searchByMovie(Long movieId);
+    List<Review> searchByMovie(Long movieId, Pageable pageable);
 }
